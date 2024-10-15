c3adbc81bc1b41f57bd4248439621da55e916dff
Harshit Chaudhary <83287475+harshitchaudhary1@users.noreply.github.com>
2024-10-16 01:56:29 +0530

    signin

--- Backend/tradeit (1)/tradeit/src/main/java/com/trade/controller/AuthController.java
+++ Backend/tradeit (1)/tradeit/src/main/java/com/trade/controller/AuthController.java
@@ -1,13 +1,18 @@
 package com.trade.controller;
 
+import com.trade.config.JwtProvider;
 import com.trade.model.User;
 import com.trade.repository.UserRepository;
+import com.trade.response.AuthResponse;
+import com.trade.service.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +23,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
 
     @PostMapping("/signup")
-    public ResponseEntity<User> register(@RequestBody User user) throws Exception {
+    public ResponseEntity<AuthResponse> register(@RequestBody User user) throws Exception {
 
         User isEmailExist = userRepository.findByEmail(user.getEmail());
         if(isEmailExist!=null){
@@ -40,7 +47,44 @@ public class AuthController {
         );
         SecurityContextHolder.getContext().setAuthentication(auth);
 
+        String jwt = JwtProvider.generateToken(auth);
 
-        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+        AuthResponse res = new AuthResponse();
+        res.setJwt(jwt);
+        res.setStatus(true);
+        res.setMessage("register success");
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> login(@RequestBody User user) throws Exception {
+        String userName = user.getEmail();
+        String password = user.getPassword();
+
+        Authentication auth = authenticate(userName,password);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        String jwt = JwtProvider.generateToken(auth);
+
+        AuthResponse res = new AuthResponse();
+        res.setJwt(jwt);
+        res.setStatus(true);
+        res.setMessage("login success");
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+    }
+
+    private Authentication authenticate(String userName, String password) {
+        UserDetails userDetails=customUserDetailService.loadUserByUsername(userName);
+
+        if(userDetails==null){
+            throw new BadCredentialsException("Invalid username");
+        }
+        if(!password.equals(userDetails.getPassword())){
+            throw new BadCredentialsException("Invalid password");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails,password,userDetails.getAuthorities());
+
     }
 }
---
+++ Backend/tradeit (1)/tradeit/src/main/java/com/trade/response/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.trade.response;
+
+import lombok.Data;
+
+@Data
+public class AuthResponse {
+    private String jwt;
+    private boolean status;
+    private String message;
+    private boolean isTwoFactorAuthEnable;
+    private String session;
+}
